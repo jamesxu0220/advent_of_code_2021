3f83a92baa262932a6056aa3202fc1ef1a1fb848
James Xu <zx2370@columbia.edu>
2022-10-28 14:01:46 -0400

    Finished day1 part2

--- day_one/day_one_part_one.py
+++ day_one/day_one_part_one.py
@@ -1,14 +1,18 @@
-def day_one_solution():
+def get_input():
     input = []
     with open('day_one/input.txt', 'r') as f:
         data = f.readlines()
         for line in data:
             input.append(int(line.strip()))
+    return input
+
+def day_one_solution_one():
+    input = get_input()
     res = 0
     for i in range(1, len(input)):
         if input[i] > input[i-1]:
             res += 1
     return res
     
-print(day_one_solution())
+print(day_one_solution_one())
 ## Output is 1681
---
+++ day_one/day_one_part_two.py
@@ -0,0 +1,15 @@
+from day_one_part_one import get_input
+
+def day_one_solution_two():
+    input = get_input()
+    res = 0
+    cur_sum = sum(input[:3])
+    for i in range(3, len(input)):
+        new_sum = cur_sum - input[i-3] + input[i]
+        if new_sum > cur_sum:
+            res += 1
+        cur_sum = new_sum
+    return res
+    
+print(day_one_solution_two())
+## Output is 1704
